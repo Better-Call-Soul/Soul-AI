5115fb7ab548cca297ed5d73dd68af1d9c329a91
Eslam Ashraf <71986226+EslamAsHhraf@users.noreply.github.com>
2024-02-17 16:06:52 +0200

    add data set

---
+++ models/dialogue_summarization/dataset.py
@@ -0,0 +1,17 @@
+class DialogDataSet:
+    dataset=""
+    def __init__(self, dataset):
+        self.dataset = dataset
+    
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    dataset = DialogDataSet("../../data/raw/summarization/samsum/samsum-test.csv")
