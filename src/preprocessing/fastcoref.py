bb41c8c3333d2af7ab478be85e51ff0bb0c1f006
Eslam Ashraf <71986226+EslamAsHhraf@users.noreply.github.com>
2024-04-15 15:45:02 +0200

    update links which i upload from

--- src/preprocessing/fastcoref.py
+++ src/preprocessing/fastcoref.py
@@ -9,7 +9,7 @@ class Fastcoref:
     def __init__(self):
         # self.nlp_fastcoref = spacy.load("en_core_web_sm")
         # self.nlp_fastcoref.add_pipe("fastcoref")
-        self.nlp_fastcoref = spacy.load("/content/drive/MyDrive/GP/preprocessing/fastcoref")
+        self.nlp_fastcoref = spacy.load("/content/drive/MyDrive/GP/Graduation-Project/models/preprocessing/fastcoref")
    
     def coreference_resolution(self,text:str)->str:
         doc =self.nlp_fastcoref(      # for multiple texts use nlp.pipe
